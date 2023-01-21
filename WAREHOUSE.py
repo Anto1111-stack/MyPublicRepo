f1b92f394e4e3091c370900db2a6065fb44f9783
Antonio Vrsalovic <anto.vrsalovic@gmail.com>
2023-01-21 12:56:14 +0000

    Added some comments in the main file

--- WAREHOUSE.py
+++ WAREHOUSE.py
@@ -1,9 +1,12 @@
 
 from mymodule import product_pick,hello_operator
  
-# Main bulk of the code in the 'mymodule' module and here we just call this two functions!
+# Main bulk of the code in the 'mymodule' module and here we 
+# just call this two functions!
 
+# Hello Operator
 hello_operator()
 
+# To make the code flash and input the code
 product_pick()
 
